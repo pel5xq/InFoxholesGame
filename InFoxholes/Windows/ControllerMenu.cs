11b96e1990fad58e3c29dce494362a15904f7ac9
pel5xq <pel5xq@virginia.edu>
2014-03-27 11:59:58 -0400

    Improved Menu
    Yet to add sound/graphics to main menu.

---
+++ InFoxholes/Windows/ControllerMenu.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace InFoxholes.Windows
+{
+    public class ControllerMenu
+     {
+        public Texture2D menuTexture;
+        public Texture2D controllerMenuTexture;
+        Vector2 topleft;
+        Vector2 botright;
+        public Texture2D pixel;
+        bool hoverFlag = false;
+        bool controllerFlag = false;
+
+        /* Magic Numbers */
+        static int startButtonLX = 345;
+        static int startButtonLY = 415;
+        static int startButtonRX = 470;
+        static int startButtonRY = 465;
+        int startButtonWidth = startButtonRX - startButtonLX;
+        int startButtonHeight = startButtonRY - startButtonLY;
+        private float lineThickness = 4f;
+        private float halfPi = (float)(Math.PI / 2);
+                
+
+        public ControllerMenu(ContentManager Content, SpriteBatch spriteBatch)
+        {
+            menuTexture = Content.Load<Texture2D>("Graphics\\Menu");
+            controllerMenuTexture = Content.Load<Texture2D>("Graphics\\ControllerMenu");
+            topleft = new Vector2(startButtonLX, startButtonLY);
+            botright = new Vector2(startButtonRX, startButtonRY);
+            pixel = new Texture2D(spriteBatch.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            pixel.SetData(new[] { Color.White });
+        }
+        public void Update(GameTime gameTime, GamePadState gamepadState)
+        {
+            if (gamepadState.IsConnected)
+            {
+                hoverFlag = true;
+                controllerFlag = true;
+                if (gamepadState.Buttons.A == ButtonState.Pressed && MainGame.previousGamepadState.Buttons.A != ButtonState.Pressed)
+                {
+                    Menu.isInControllerMenu = false;
+                }
+            }
+            else
+            {
+                controllerFlag = false;
+                if (overStartButton(Mouse.GetState().X, Mouse.GetState().Y))
+                {
+                    hoverFlag = true;
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed && MainGame.previousMouseState.LeftButton != ButtonState.Pressed)
+                    {
+                        Menu.isInControllerMenu = false;
+                    }
+                }
+                else
+                {
+                    hoverFlag = false;
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            if (controllerFlag) spriteBatch.Draw(controllerMenuTexture, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            else spriteBatch.Draw(menuTexture, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            if (hoverFlag)
+            {
+                spriteBatch.Draw(pixel, topleft, null, Color.Black, 0, Vector2.Zero, new Vector2(startButtonWidth, lineThickness),
+                    SpriteEffects.None, 0);
+                spriteBatch.Draw(pixel, topleft, null, Color.Black, halfPi, Vector2.Zero, new Vector2(startButtonHeight, lineThickness),
+                    SpriteEffects.None, 0);
+                spriteBatch.Draw(pixel, botright, null, Color.Black, 2 * halfPi, Vector2.Zero, new Vector2(startButtonWidth, lineThickness),
+                    SpriteEffects.None, 0);
+                spriteBatch.Draw(pixel, botright, null, Color.Black, -1 * halfPi, Vector2.Zero, new Vector2(startButtonHeight, lineThickness),
+                    SpriteEffects.None, 0);
+            }
+        }
+        private bool overStartButton(int mouseX, int mouseY)
+        {
+            if (mouseX <= startButtonRX && mouseX >= startButtonLX
+                && mouseY <= startButtonRY && mouseY >= startButtonLY)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- InFoxholes/Windows/Menu.cs
+++ InFoxholes/Windows/Menu.cs
@@ -3,88 +3,179 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using InFoxholes.Util;
 
 
 namespace InFoxholes.Windows
 {
-    class Menu
+    public class Menu
     {
-        public Texture2D menuTexture;
-        public Texture2D controllerMenuTexture;
+        public AnimatedSprite menuTexture;
         Vector2 topleft;
         Vector2 botright;
+        Vector2 topleftControls;
+        Vector2 botrightControls;
         public Texture2D pixel;
         bool hoverFlag = false;
-        bool controllerFlag = false;
+        bool hoverFlagControls = false;
+        public static bool isInControllerMenu;
+        public ControllerMenu controllerMenu;
+        int selectedButton;
 
         /* Magic Numbers */
-        static int startButtonLX = 345;
-        static int startButtonLY = 415;
-        static int startButtonRX = 470;
-        static int startButtonRY = 465;
+        static int startButtonLX = 30;
+        static int startButtonLY = 135;
+        static int startButtonRX = 120;
+        static int startButtonRY = 175;
+        static int controlsButtonLX = 30;
+        static int controlsButtonLY = 205;
+        static int controlsButtonRX = 180;
+        static int controlsButtonRY = 240;
         int startButtonWidth = startButtonRX - startButtonLX;
         int startButtonHeight = startButtonRY - startButtonLY;
+        int controlsButtonWidth = controlsButtonRX - controlsButtonLX;
+        int controlsButtonHeight = controlsButtonRY - controlsButtonLY;
         private float lineThickness = 4f;
         private float halfPi = (float)(Math.PI / 2);
+        int animationSpeed = 100;
+        float thresholdLength = .8f;
                 
 
         public Menu(ContentManager Content, SpriteBatch spriteBatch)
         {
-            menuTexture = Content.Load<Texture2D>("Graphics\\Menu");
-            controllerMenuTexture = Content.Load<Texture2D>("Graphics\\ControllerMenu");
+            menuTexture = new AnimatedSprite(Content.Load<Texture2D>("Graphics\\MainMenu"), 2, 2, animationSpeed);
             topleft = new Vector2(startButtonLX, startButtonLY);
             botright = new Vector2(startButtonRX, startButtonRY);
+            topleftControls = new Vector2(controlsButtonLX, controlsButtonLY);
+            botrightControls = new Vector2(controlsButtonRX, controlsButtonRY);
             pixel = new Texture2D(spriteBatch.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
             pixel.SetData(new[] { Color.White });
+            isInControllerMenu = false;
+            controllerMenu = new ControllerMenu(Content, spriteBatch);
+            selectedButton = 0;
         }
         public void Update(GameTime gameTime, GamePadState gamepadState)
         {
-            if (gamepadState.IsConnected)
+            if (isInControllerMenu)
             {
-                controllerFlag = true;
-                if (gamepadState.Buttons.A == ButtonState.Pressed)
-                {
-                    MainGame.isInMenu = false;
-                }
+                controllerMenu.Update(gameTime, gamepadState);
             }
             else
             {
-                controllerFlag = false;
-            }
-
-            if (overStartButton(Mouse.GetState().X, Mouse.GetState().Y))
-            {
-                hoverFlag = true;
-                if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                if (gamepadState.IsConnected)
                 {
-                    MainGame.isInMenu = false;
+                    if (selectedButton == 0)
+                    {
+                        hoverFlag = true;
+                        hoverFlagControls = false;
+                        if (gamepadState.Buttons.A == ButtonState.Pressed && MainGame.previousGamepadState.Buttons.A != ButtonState.Pressed)
+                        {
+                            MainGame.isInMenu = false;
+                        }
+                        if (gamepadState.DPad.Down == ButtonState.Pressed  && MainGame.previousGamepadState.DPad.Down != ButtonState.Pressed ||
+                            gamepadState.DPad.Up == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Up != ButtonState.Pressed ||
+                            gamepadState.DPad.Left == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Left != ButtonState.Pressed ||
+                            gamepadState.DPad.Right == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Right != ButtonState.Pressed ||
+                            gamepadState.ThumbSticks.Left.Length() > thresholdLength && !(MainGame.previousGamepadState.ThumbSticks.Left.Length() > thresholdLength))
+                        {
+                            selectedButton = 1;
+                        }
+                    }
+                    else if (selectedButton == 1)
+                    {
+                        hoverFlag = false;
+                        hoverFlagControls = true;
+                        if (gamepadState.Buttons.A == ButtonState.Pressed && MainGame.previousGamepadState.Buttons.A != ButtonState.Pressed)
+                        {
+                            isInControllerMenu = true;
+                        }
+                        if (gamepadState.DPad.Down == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Down != ButtonState.Pressed ||
+                            gamepadState.DPad.Up == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Up != ButtonState.Pressed ||
+                            gamepadState.DPad.Left == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Left != ButtonState.Pressed ||
+                            gamepadState.DPad.Right == ButtonState.Pressed && MainGame.previousGamepadState.DPad.Right != ButtonState.Pressed ||
+                            gamepadState.ThumbSticks.Left.Length() > thresholdLength && !(MainGame.previousGamepadState.ThumbSticks.Left.Length() > thresholdLength))
+                        {
+                            selectedButton = 0;
+                        }
+                    }
                 }
-            }
-            else
-            {
-                hoverFlag = false;
+                else
+                {
+                    if (overStartButton(Mouse.GetState().X, Mouse.GetState().Y))
+                    {
+                        hoverFlag = true;
+                        if (Mouse.GetState().LeftButton == ButtonState.Pressed && MainGame.previousMouseState.LeftButton != ButtonState.Pressed)
+                        {
+                            MainGame.isInMenu = false;
+                        }
+                    }
+                    else
+                    {
+                        hoverFlag = false;
+                    }
+                    if (overControlsButton(Mouse.GetState().X, Mouse.GetState().Y))
+                    {
+                        hoverFlagControls = true;
+                        if (Mouse.GetState().LeftButton == ButtonState.Pressed && MainGame.previousMouseState.LeftButton != ButtonState.Pressed)
+                        {
+                            isInControllerMenu = true;
+                        }
+                    }
+                    else
+                    {
+                        hoverFlagControls = false;
+                    }
+                }
+                
             }
         }
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            if (controllerFlag) spriteBatch.Draw(controllerMenuTexture, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-            else spriteBatch.Draw(menuTexture, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-            if (hoverFlag)
+            if (isInControllerMenu)
             {
-                spriteBatch.Draw(pixel, topleft, null, Color.Black, 0, Vector2.Zero, new Vector2(125, lineThickness),
-                    SpriteEffects.None, 0);
-                spriteBatch.Draw(pixel, topleft, null, Color.Black, halfPi, Vector2.Zero, new Vector2(50, lineThickness),
-                    SpriteEffects.None, 0);
-                spriteBatch.Draw(pixel, botright, null, Color.Black, 2 * halfPi, Vector2.Zero, new Vector2(125, lineThickness),
-                    SpriteEffects.None, 0);
-                spriteBatch.Draw(pixel, botright, null, Color.Black, -1 * halfPi, Vector2.Zero, new Vector2(50, lineThickness),
-                    SpriteEffects.None, 0);
+                controllerMenu.Draw(spriteBatch, gameTime);
+            }
+            else
+            {
+                menuTexture.Update();
+                menuTexture.Draw(spriteBatch, Vector2.Zero,  1f, SpriteEffects.None);
+                if (hoverFlag)
+                {
+                    spriteBatch.Draw(pixel, topleft, null, Color.Black, 0, Vector2.Zero, new Vector2(startButtonWidth, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, topleft, null, Color.Black, halfPi, Vector2.Zero, new Vector2(startButtonHeight, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, botright, null, Color.Black, 2 * halfPi, Vector2.Zero, new Vector2(startButtonWidth, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, botright, null, Color.Black, -1 * halfPi, Vector2.Zero, new Vector2(startButtonHeight, lineThickness),
+                        SpriteEffects.None, 0);
+                }
+                if (hoverFlagControls)
+                {
+                    spriteBatch.Draw(pixel, topleftControls, null, Color.Black, 0, Vector2.Zero, new Vector2(controlsButtonWidth, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, topleftControls, null, Color.Black, halfPi, Vector2.Zero, new Vector2(controlsButtonHeight, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, botrightControls, null, Color.Black, 2 * halfPi, Vector2.Zero, new Vector2(controlsButtonWidth, lineThickness),
+                        SpriteEffects.None, 0);
+                    spriteBatch.Draw(pixel, botrightControls, null, Color.Black, -1 * halfPi, Vector2.Zero, new Vector2(controlsButtonHeight, lineThickness),
+                        SpriteEffects.None, 0);
+                }
             }
         }
         private bool overStartButton(int mouseX, int mouseY)
         {
             if (mouseX <= startButtonRX && mouseX >= startButtonLX
                 && mouseY <= startButtonRY && mouseY >= startButtonLY)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool overControlsButton(int mouseX, int mouseY)
+        {
+            if (mouseX <= controlsButtonRX && mouseX >= controlsButtonLX
+                && mouseY <= controlsButtonRY && mouseY >= controlsButtonLY)
             {
                 return true;
             }
