64fa0b8d69b5615e474fcd627d36024c0db52088
pel5xq <pel5xq@virginia.edu>
2014-03-28 12:39:54 -0400

    Added Gunshot Sounds

--- InFoxholes/Weapons/SniperRifle.cs
+++ InFoxholes/Weapons/SniperRifle.cs
@@ -3,6 +3,8 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using InFoxholes.Waves;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
 
 
 namespace InFoxholes.Weapons
@@ -28,6 +30,7 @@ namespace InFoxholes.Weapons
             reloadCooldown = reloadLength;
             WeaponTexture = Content.Load<Texture2D>("Graphics\\LeeEnfield");
             bullet = Content.Load<Texture2D>("Graphics\\LeeEnfieldAmmo");
+            shotSound = Content.Load<SoundEffect>("Music\\rifle.wav");
             base.Initialize(Content, spriteBatch, HUDPosition, ammosupply, manager);
         }
         override public void DrawHUD(SpriteBatch spriteBatch, GameTime gameTime)
@@ -36,6 +39,16 @@ namespace InFoxholes.Weapons
             if (isFireable(gameTime)) spriteBatch.Draw(WeaponTexture, hudPosition, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             else spriteBatch.Draw(WeaponTexture, hudPosition, null, Color.White * disabledAlpha, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             base.DrawHUD(spriteBatch, gameTime);
+        }
+
+        public override void playShot(GameTime currentTime)
+        {
+            if (clipSupply == 0 && ammoSupply == 0) emptyClipSound.Play();
+            else if (!reloadOver(currentTime)) emptyClipSound.Play();
+            else
+            {
+                if (isFireable(currentTime)) shotSound.Play();
+            }
         }
 
     }
