b9313d041b71347698176c0228c7bb4591ca96ac
Sravan Tumuluri <skt3rt@virginia.edu>
2014-04-14 20:11:34 -0400

    New Waves

--- InFoxholes/Waves/TutorialWave3.cs
+++ InFoxholes/Waves/TutorialWave3.cs
@@ -31,17 +31,17 @@ namespace InFoxholes.Waves
             enemiesToSpawn.Insert(0, new Enemy1());
             spawnTimings.Insert(0, baseTime + 0 * interval);
             enemiesToSpawn.Insert(1, new Enemy1());
-            spawnTimings.Insert(1, baseTime + 1 * interval);
+            spawnTimings.Insert(1, baseTime + 1.5 * interval);
             enemiesToSpawn.Insert(2, new HeadShotTest());
-            spawnTimings.Insert(2, baseTime + 2 * interval);
+            spawnTimings.Insert(2, baseTime + 2.5 * interval);
             enemiesToSpawn.Insert(3, new Enemy1());
-            spawnTimings.Insert(3, baseTime + 2 * interval);
+            spawnTimings.Insert(3, baseTime + 4 * interval);
             enemiesToSpawn.Insert(4, new HeadShotTest());
-            spawnTimings.Insert(4, baseTime + 3 * interval);
+            spawnTimings.Insert(4, baseTime + 5 * interval);
             enemiesToSpawn.Insert(5, new Enemy1());
-            spawnTimings.Insert(5, baseTime + 4 * interval);
+            spawnTimings.Insert(5, baseTime + 6 * interval);
             enemiesToSpawn.Insert(6, new Enemy1());
-            spawnTimings.Insert(6, baseTime + 5 * interval);
+            spawnTimings.Insert(6, baseTime + 7 * interval);
            // }
             enemiesToSpawn.Insert(7, new HeadShotTest());
             spawnTimings.Insert(7, baseTime + 6 * interval);
