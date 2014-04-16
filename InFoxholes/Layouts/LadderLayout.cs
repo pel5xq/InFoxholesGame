0d1541ae44eeeaf15916c28b9df21a4213ea9451
unknown <wre9fz@RH340-13.lab.cs.virginia.edu>
2014-04-15 21:19:48 -0400

    Added ladder layout and pather

---
+++ InFoxholes/Layouts/LadderLayout.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using InFoxholes.Util;
+
+namespace InFoxholes.Layouts
+{
+    public class LadderLayout : Layout
+    {
+        bool spawnSwitch;
+
+        /* Magic Numbers */
+        Vector2 enemySpawns = new Vector2(280, 465); 
+        List<Vector2> scavengerSpawns = new List<Vector2>() { 
+            new Vector2(15, 121), 
+            new Vector2(55, 121), //225, 182
+            new Vector2(95, 121), 
+            new Vector2(135, 121) };
+        List<Vector2> HUDPositions = new List<Vector2>() { 
+            new Vector2(10, 10), 
+            new Vector2(448, 10), 
+            new Vector2(10, 50), 
+            new Vector2(10, 100) };
+        Vector2 offscreenposition = new Vector2(-150, -150);
+        Vector2 countdownposition = new Vector2(475f, 50f);
+        Vector2 weapongunpoint = new Vector2(150, 100);
+        Vector2 weaponposition = new Vector2(-1000, -1000);
+        Vector2 scavengerspawnposition = new Vector2(135, 90);
+        float angleadjust = .25f;
+        float distanceadjust = -15f;
+        float burstadjustX = -5;
+        float burstadjustY = -5;
+        float crosshairadjustX = -15;
+        float crosshairadjustY = -15;
+
+        public LadderLayout()
+        {
+            pather = new LadderPather();
+            offscreenPosition = offscreenposition;
+            countdownPosition = countdownposition;
+            weaponPosition = weaponposition;
+            weaponGunpoint = weapongunpoint;
+            scavengerSpawnPosition = scavengerspawnposition;
+            angleAdjust = angleadjust;
+            distanceAdjust = distanceadjust;
+            spawnSwitch = false;
+            burstAdjustX = burstadjustX;
+            burstAdjustY = burstadjustY;
+            crosshairAdjustX = crosshairadjustX;
+            crosshairAdjustY = crosshairadjustY;
+        }
+        public override void Initialize(ContentManager Content)
+        {
+            layoutBackdrop = new AnimatedSprite(Content.Load<Texture2D>("Graphics\\LadderLevel"), 1, 1, 10);
+            gameOverSprite = new AnimatedSprite(Content.Load<Texture2D>("Graphics\\LadderGameOver"), 1, 1, 10);
+            base.Initialize(Content);
+        }
+        public override Vector2 getScavengerTrenchPlacement(int seat)
+        {
+            if (seat < scavengerSpawns.Count) return scavengerSpawns[seat];
+            else return scavengerSpawns[scavengerSpawns.Count - 1];
+        }
+        public override Vector2 getHUDPlacement(int seat)
+        {
+            if (seat < HUDPositions.Count) return HUDPositions[seat];
+            else return HUDPositions[HUDPositions.Count - 1];
+        }
+        public override bool checkAimingVector(Vector2 aimingVector)
+        {
+            //return aimingVector.Y > 0;
+            return true;
+        }
+        public override Vector2 enemySpawnPoint()
+        {
+            return enemySpawns;
+        }
+        public override bool isOnGround(Vector2 position, int width, int height)
+        {
+            return position.Y >= 90; 
+        }
+    }
+}
---
+++ InFoxholes/Layouts/LadderPather.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+
+namespace InFoxholes.Layouts
+{
+    public class LadderPather : Pather
+    {
+        /* Magic numbers */
+        static int halfwayX = 335;
+        Vector2 trenchentranceposition = new Vector2(1000,1000);
+        int trenchentrancewidth = 115;
+        int trenchentranceheight = 60;
+        Vector2 columnPosition = new Vector2(265, 115);
+        int columnWidth = 70;
+        int columnHeight = 300;
+        int endOfRoadY = 410;
+        float xFactor = .94F;
+        float yFactor = .34F; 
+
+        public override void Initialize()
+        {
+            trenchEntrancePosition = trenchentranceposition;
+            trenchEntranceWidth = trenchentrancewidth;
+            trenchEntranceHeight = trenchentranceheight;
+            base.Initialize();
+        }
+        public override Vector2 Move(Vector2 unitPosition, bool isTowardsTrench, float speed)
+        {
+            Vector2 result = new Vector2();
+            // if in first section of ladder
+            if (unitPosition.Y > 295)
+            {
+                if (!isTowardsTrench)
+                {
+                    result.X = unitPosition.X - (xFactor*speed);
+                    result.Y = unitPosition.Y + (yFactor * speed);
+                }
+                else
+                {
+                    result.X = unitPosition.X + (xFactor*speed);
+                    result.Y = unitPosition.Y - (yFactor * speed);
+                }
+            }
+            // if in second section of ladder
+            else if (unitPosition.Y > 185)
+            {
+                if (!isTowardsTrench)
+                {
+                    result.X = unitPosition.X + (xFactor * speed);
+                    result.Y = unitPosition.Y + (yFactor * speed);
+                }
+                else
+                {
+                    result.X = unitPosition.X - (xFactor * speed);
+                    result.Y = unitPosition.Y - (yFactor * speed);
+                }
+            }
+            // if in third section of ladder
+            else if (unitPosition.Y > 90)
+            {
+                if (!isTowardsTrench)
+                {
+                    result.X = unitPosition.X - (xFactor * speed);
+                    result.Y = unitPosition.Y + (yFactor * speed);
+                }
+                else
+                {
+                    result.X = unitPosition.X + (xFactor * speed);
+                    result.Y = unitPosition.Y - (yFactor * speed);
+                }
+            }
+            // if on top platform
+            else
+            {
+                if (!isTowardsTrench)
+                {
+                    if (unitPosition.X > 700)
+                    {
+                        result.X = unitPosition.X - (speed * xFactor);
+                        result.Y = unitPosition.Y + (speed * yFactor);
+                    }
+                    else
+                    {
+                        result.X = unitPosition.X + speed;
+                        result.Y = unitPosition.Y;
+                    }
+                }
+                else
+                {
+                    result.X = unitPosition.X - speed;
+                    result.Y = unitPosition.Y; 
+                }
+            }
+            return result;
+        }
+        public override bool isForward(Vector2 unitPosition, bool isTowardsTrench, float speed)
+        {
+            if (unitPosition.Y > 295)
+            {
+                if (isTowardsTrench)
+                {
+                    return true; 
+                }
+                else
+                {
+                    return false; 
+                }
+            }
+            // if in second section of ladder
+            else if (unitPosition.Y > 185)
+            {
+                if (isTowardsTrench)
+                {
+                    return false; 
+                }
+                else
+                {
+                    return true; 
+                }
+            }
+            // if in third section of ladder
+            else if (unitPosition.Y > 90)
+            {
+                if (isTowardsTrench)
+                {
+                    return true; 
+                }
+                else
+                {
+                    return false; 
+                }
+            }
+            // if on top platform
+            else
+            {
+                if (isTowardsTrench)
+                {
+                    return false; 
+                }
+                else
+                {
+                    return true; 
+                }
+            }
+
+
+
+        }
+    }
+}
