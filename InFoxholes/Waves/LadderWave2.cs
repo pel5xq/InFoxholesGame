d039bff5e6ceb70cc853bb12e0b96a6f81ad50f6
pel5xq <pel5xq@virginia.edu>
2014-04-16 15:45:53 -0400

    Added 2 Other Blank Ladder Waves

---
+++ InFoxholes/Waves/LadderWave2.cs
@@ -0,0 +1,37 @@
+﻿using InFoxholes.Enemies;
+using InFoxholes.Layouts;
+using InFoxholes.Looting;
+using InFoxholes.Friendlies;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace InFoxholes.Waves
+{
+    public class LadderWave2 : Wave
+    {
+
+        /* Magic Numbers */
+        int wavesize = 1;
+        double baseTime = 1500;
+        double interval = 8000;
+
+        override public void Initialize(ContentManager content, WaveManager manager)
+        {
+            infiniteAmmoModeOn = false;
+            infiniteFoodModeOn = true;
+            isTutorialWave = false;
+            waveSize = wavesize;
+            spawnTimings = new List<double>(waveSize);
+            enemiesToSpawn = new List<Enemy>(waveSize);
+            lootList = new List<Loot>(waveSize);
+            enemiesToSpawn.Insert(0, new HeadShotTest());
+            spawnTimings.Insert(0, baseTime + 0 * interval);
+            lootList.Add(new SniperAmmoLoot(2, content));
+            openingTextFilename = "Content//Text//LadderWave2.txt";
+            layout = new LadderLayout();
+            base.Initialize(content, manager);
+        }
+    }
+}
