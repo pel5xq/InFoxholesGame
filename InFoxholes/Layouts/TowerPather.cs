4b7097556bce36477fba7294deb27b7508e8ace1
pel5xq <pel5xq@virginia.edu>
2014-04-16 19:07:25 -0400

    Fixed Tower Pather

--- InFoxholes/Layouts/TowerPather.cs
+++ InFoxholes/Layouts/TowerPather.cs
@@ -6,6 +6,8 @@ namespace InFoxholes.Layouts
 {
     public class TowerPather : Pather
     {
+        private static Random rnd = new Random();
+
         /* Magic numbers */
         static int halfwayX = 335;
         Vector2 trenchentranceposition = new Vector2(280, 165);
@@ -26,44 +28,76 @@ namespace InFoxholes.Layouts
         public override Vector2 Move(Vector2 unitPosition, bool isTowardsTrench, float speed)
         {
             Vector2 result = new Vector2();
-            if (intersectsWith(unitPosition, 1, 1, columnPosition, columnWidth, columnHeight))
+            if (isTowardsTrench)
             {
-                result.X = unitPosition.X;
-                if (isTowardsTrench) result.Y = unitPosition.Y - speed;
-                else result.Y = unitPosition.Y + speed;
-            }
-            else
-            {
-                result.Y = unitPosition.Y;
-                if (unitPosition.Y >= endOfRoadY)
+                if (intersectsWith(unitPosition, 1, 1, columnPosition, columnWidth, columnHeight))
                 {
                     result.X = unitPosition.X;
-                    if (isTowardsTrench)
-                    {
-                        result.Y = unitPosition.Y - speed;
-                    }
-                }
-                else if (isTowardsTrench)
-                {
-                    if (isForward(unitPosition, isTowardsTrench, speed)) 
-                    {
-                        result.X = unitPosition.X + speed;
-                    }
-                    else 
-                    {
-                        result.X = unitPosition.X - speed;
-                    }
+                    if (isTowardsTrench) result.Y = unitPosition.Y - speed;
+                    else result.Y = unitPosition.Y + speed;
                 }
                 else
                 {
-                    if (isForward(unitPosition, isTowardsTrench, speed))
+                    result.Y = unitPosition.Y;
+                    if (unitPosition.Y >= endOfRoadY)
+                    {
+                        result.X = unitPosition.X;
+                        if (isTowardsTrench)
+                        {
+                            result.Y = unitPosition.Y - speed;
+                        }
+                    }
+                    else if (isTowardsTrench)
+                    {
+                        if (isForward(unitPosition, isTowardsTrench, speed))
+                        {
+                            result.X = unitPosition.X + speed;
+                        }
+                        else
+                        {
+                            result.X = unitPosition.X - speed;
+                        }
+                    }
+                    else
+                    {
+                        if (isForward(unitPosition, isTowardsTrench, speed))
+                        {
+                            result.X = unitPosition.X - speed;
+                        }
+                        else
+                        {
+                            result.X = unitPosition.X + speed;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (unitPosition.Y < 380)
+                {
+                    result.X = unitPosition.X;
+                    result.Y = unitPosition.Y + speed;
+                }
+                else
+                {
+                    if (unitPosition.X < 335)
                     {
                         result.X = unitPosition.X - speed;
+                        result.Y = unitPosition.Y;
+                    }
+                    else if (unitPosition.X > 335)
+                    {
+                        result.X = unitPosition.X + speed;
+                        result.Y = unitPosition.Y;
                     }
                     else
                     {
                         result.X = unitPosition.X + speed;
+                        result.Y = unitPosition.Y;
+                        if (rnd.Next(2) == 0) result.X = unitPosition.X + speed;
+                        else result.X = unitPosition.X - speed;
                     }
+                    
                 }
             }
             return result;
