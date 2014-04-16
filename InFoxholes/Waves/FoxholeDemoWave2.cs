080894bc6ffc78ecc092de047f48d7e617a4f016
unknown <wre9fz@RH340-06.lab.cs.virginia.edu>
2014-04-16 18:36:33 -0400

    Improvements on demo waves

--- InFoxholes/Waves/FoxholeDemoWave2.cs
+++ InFoxholes/Waves/FoxholeDemoWave2.cs
@@ -12,7 +12,7 @@ namespace InFoxholes.Waves
     {
 
         /* Magic Numbers */
-        int wavesize = 8;
+        int wavesize = 9;
         double baseTime = 1500;
         double interval = 8000;
 
@@ -28,22 +28,24 @@ namespace InFoxholes.Waves
             enemiesToSpawn = new List<Enemy>(waveSize);
             lootList = new List<Loot>(waveSize);
             enemiesToSpawn.Insert(0, new Enemy1());
-            spawnTimings.Insert(0, baseTime + 0 * interval);
-            enemiesToSpawn.Insert(1, new Enemy1());
-            spawnTimings.Insert(1, baseTime + 1 * interval);
-            //enemiesToSpawn.Insert(2, new HeadShotTest());
-            enemiesToSpawn.Insert(2, new DogEnemy());
-            spawnTimings.Insert(2, baseTime + 2 * interval);
+            spawnTimings.Insert(0, baseTime);
+            enemiesToSpawn.Insert(1, new HeadShotTest());
+            spawnTimings.Insert(1, baseTime + 1.5 * interval);
+            enemiesToSpawn.Insert(2, new HeadShotTest());
+            spawnTimings.Insert(2, baseTime + 2.5 * interval);
             enemiesToSpawn.Insert(3, new Enemy1());
-            spawnTimings.Insert(3, baseTime + 3.5 * interval);
+            spawnTimings.Insert(3, baseTime + 3 * interval);
             enemiesToSpawn.Insert(4, new HeadShotTest());
             spawnTimings.Insert(4, baseTime + 4.5 * interval);
-            enemiesToSpawn.Insert(5, new Enemy1());
-            spawnTimings.Insert(5, baseTime + 6 * interval);
-            enemiesToSpawn.Insert(6, new Enemy1());
-            spawnTimings.Insert(6, baseTime + 7.5 * interval);
+            enemiesToSpawn.Insert(5, new DogEnemy());
+            spawnTimings.Insert(5, baseTime + 5.5 * interval);
+            //enemiesToSpawn.Insert(2, new HeadShotTest());
+            enemiesToSpawn.Insert(6, new DogEnemy());
+            spawnTimings.Insert(6, baseTime + 6.5 * interval);
             enemiesToSpawn.Insert(7, new DogEnemy());
-            spawnTimings.Insert(7, baseTime + 8 * interval);
+            spawnTimings.Insert(7, baseTime + 6.5 * interval + 1000);
+            enemiesToSpawn.Insert(8, new DogEnemy());
+            spawnTimings.Insert(8, baseTime + 6.5 * interval + 2000);
             lootList.Add(new SniperAmmoLoot(2, content));
             lootList.Add(new SniperAmmoLoot(2, content));
             lootList.Add(new MachineGunAmmoLoot(10, content));
@@ -52,8 +54,8 @@ namespace InFoxholes.Waves
             lootList.Add(new SniperAmmoLoot(4, content));
             lootList.Add(new MachineGunAmmoLoot(6, content));
             lootList.Add(new SniperAmmoLoot(2, content));
-
-            openingTextFilename = "Content//Text//FoxholeDemoWave1.txt";
+            lootList.Add(new SniperAmmoLoot(0, content));
+            openingTextFilename = "Content//Text//FoxholeDemoWave2.txt";
             layout = new FoxholeLayout();
             base.Initialize(content, manager);
         }
--- InFoxholes/Waves/TowerDemoWave2.cs
+++ InFoxholes/Waves/TowerDemoWave2.cs
@@ -12,7 +12,7 @@ namespace InFoxholes.Waves
     {
 
         /* Magic Numbers */
-        int wavesize = 8;
+        int wavesize = 10;
         double baseTime = 1500;
         double interval = 8000;
 
@@ -27,23 +27,27 @@ namespace InFoxholes.Waves
             spawnTimings = new List<double>(waveSize);
             enemiesToSpawn = new List<Enemy>(waveSize);
             lootList = new List<Loot>(waveSize);
-            enemiesToSpawn.Insert(0, new Enemy1());
+            enemiesToSpawn.Insert(0, new HeadShotTest());
             spawnTimings.Insert(0, baseTime + 0 * interval);
-            enemiesToSpawn.Insert(1, new Enemy1());
-            spawnTimings.Insert(1, baseTime + 1 * interval);
+            enemiesToSpawn.Insert(1, new HeadShotTest());
+            spawnTimings.Insert(1, baseTime + 1.5 * interval);
             //enemiesToSpawn.Insert(2, new HeadShotTest());
-            enemiesToSpawn.Insert(2, new DogEnemy());
-            spawnTimings.Insert(2, baseTime + 2 * interval);
-            enemiesToSpawn.Insert(3, new Enemy1());
+            enemiesToSpawn.Insert(2, new ParachuteEnemy());
+            spawnTimings.Insert(2, baseTime + 2.5 * interval);
+            enemiesToSpawn.Insert(3, new ParachuteEnemy());
             spawnTimings.Insert(3, baseTime + 3.5 * interval);
-            enemiesToSpawn.Insert(4, new HeadShotTest());
+            enemiesToSpawn.Insert(4, new Enemy1());
             spawnTimings.Insert(4, baseTime + 4.5 * interval);
-            enemiesToSpawn.Insert(5, new Enemy1());
+            enemiesToSpawn.Insert(5, new HumanShieldEnemy());
             spawnTimings.Insert(5, baseTime + 6 * interval);
-            enemiesToSpawn.Insert(6, new Enemy1());
+            enemiesToSpawn.Insert(6, new ParachuteEnemy());
             spawnTimings.Insert(6, baseTime + 7.5 * interval);
-            enemiesToSpawn.Insert(7, new DogEnemy());
-            spawnTimings.Insert(7, baseTime + 8 * interval);
+            enemiesToSpawn.Insert(7, new ParachuteEnemy());
+            spawnTimings.Insert(7, baseTime + 7.5 * interval);
+            enemiesToSpawn.Insert(8, new HumanShieldEnemy());
+            spawnTimings.Insert(8, baseTime + 8 * interval);
+            enemiesToSpawn.Insert(9, new HumanShieldEnemy());
+            spawnTimings.Insert(9, baseTime + 8 * interval);
             lootList.Add(new SniperAmmoLoot(2, content));
             lootList.Add(new SniperAmmoLoot(2, content));
             lootList.Add(new MachineGunAmmoLoot(10, content));
@@ -52,9 +56,16 @@ namespace InFoxholes.Waves
             lootList.Add(new SniperAmmoLoot(4, content));
             lootList.Add(new MachineGunAmmoLoot(6, content));
             lootList.Add(new SniperAmmoLoot(2, content));
+            lootList.Add(new SniperAmmoLoot(4, content));
+            lootList.Add(new MachineGunAmmoLoot(10, content));
 
+<<<<<<< HEAD
             openingTextFilename = "Content//Text//FoxholeDemoWave1.txt";
+            layout = new TowerLayout();
+=======
+            openingTextFilename = "Content//Text//TowerDemoWave2.txt";
             layout = new FoxholeLayout();
+>>>>>>> b9d00c23889872aa2e839e7c35294805808d9bbb
             base.Initialize(content, manager);
         }
     }
